7e0138d7a8754dd23bdd00681df4b021df5e22f1
javierlmserpis <javierlmserpis@gmail.com>
2014-04-09 10:34:20 +0200

    ejercicios clases con parameter

--- PArticulo/PArticulo/Main.cs
+++ PArticulo/PArticulo/Main.cs
@@ -16,23 +16,26 @@ namespace PArticulo
 			
 			MySqlConnection mySqlConnection = new MySqlConnection(conexion);
 			mySqlConnection.Open();
+			String horaActual = DateTime.Now.ToString();
 			
 			string sql = "select * from articulo";
+			string sqlUpdate = String.Format("update articulo set nombre={0} where id=1",horaActual);
+			
 			MySqlCommand updateMySqlCommand = mySqlConnection.CreateCommand();
-			
-			//TODO
-			updateMySqlCommand.CommandText = "update articulo set nombre=nombre where id=1"; //acabar ejerc parametro
-			
-			//TODO parametro
+			updateMySqlCommand.CommandText = sqlUpdate;
 			updateMySqlCommand.ExecuteNonQuery();
-			//
+			
+			
+			
 			
 			MySqlCommand mySqlCommand = mySqlConnection.CreateCommand();
 			mySqlCommand.CommandText = sql;
-			
 			MySqlDataReader mySqlDataReader;
 			mySqlDataReader = mySqlCommand.ExecuteReader();
-    
+    		
+			
+			
+			
     	//	while (mySqlDataReader.Read()) {
       // 		Console.WriteLine(mySqlDataReader.GetString(0) + ", " + mySqlDataReader.GetString(1)+ ", "+mySqlDataReader.GetString(2));
     //		}
@@ -40,6 +43,7 @@ namespace PArticulo
 			while (mySqlDataReader.Read()){
 				Console.WriteLine("id={0} nombre={1}", mySqlDataReader["id"], mySqlDataReader["nombre"]);
 			}
+			
 			mySqlDataReader.Close();
 			mySqlConnection.Close();
 						
